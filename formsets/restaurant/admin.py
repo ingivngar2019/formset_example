1d8390b20a635acc5347c0453a0e7db411def088
unknown <Dell@DESKTOP-5KFRJD8.pgj.pgjdf.gob.mx>
2019-08-22 17:45:31 -0500

    added Receta, Ingretiente and Instruccion Models

--- formsets/restaurant/admin.py
+++ formsets/restaurant/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class RecetaAdmin(admin.ModelAdmin):
+    list_display = ('descripcion', 'id')
+
+admin.site.register(Receta, RecetaAdmin)
+
+class IngredienteAdmin(admin.ModelAdmin):
+    list_display = ('descripcion', 'receta','id')
+
+admin.site.register(Ingrediente, IngredienteAdmin)
+
+class InstruccionAdmin(admin.ModelAdmin):
+    list_display = ('descripcion', 'receta','id')
+
+admin.site.register(Instruccion, InstruccionAdmin)
--- formsets/restaurant/models.py
+++ formsets/restaurant/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Receta(models.Model):
+    titulo = models.CharField(max_length=255)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.descripcion
+
+
+class Ingrediente(models.Model):
+    receta = models.ForeignKey(Receta, on_delete=models.CASCADE)
+    descripcion = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.descripcion
+
+
+class Instruccion(models.Model):
+    receta = models.ForeignKey(Receta, on_delete=models.CASCADE)
+    numero = models.PositiveSmallIntegerField(help_text='Paso número')
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.descripcion
